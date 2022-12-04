a6328714c7d235c6d083abeef5f5e9800e2833a8
wan-ja <jgw0103@naver.com>
2022-12-04 17:03:32 +0900

    기능 몽땅 추가ㅁ

--- app/src/main/java/com/example/ItemCheck/Manage/rentalManage/CountManageActivity.java
+++ app/src/main/java/com/example/ItemCheck/Manage/rentalManage/CountManageActivity.java
@@ -3,14 +3,34 @@ package com.example.ItemCheck.Manage.rentalManage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.ItemCheck.R;
 
 public class CountManageActivity extends AppCompatActivity {
 
+    Button btn_lend;
+    EditText edt_item, edt_num;
+    ImageView grn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_count_manage);
+
+        btn_lend = findViewById(R.id.btn_lend);
+        edt_item = findViewById(R.id.edt_item);
+        edt_num = findViewById(R.id.edt_num);
+        grn = findViewById(R.id.grn);
+
+        btn_lend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
